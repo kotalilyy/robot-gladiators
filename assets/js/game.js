057735f8eeca0cb695ba7c8eabde189ea505e02a
dakota <kotalilyy@gmail.com>
2021-02-05 19:32:32 -0600

    add javascript

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,22 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+var name = "your name";
+console.log(name);
+
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+
+// this creates a function named "fight"
+// this is how you write a comment
+/*or you can
+write one
+like this
+cool,huh?*/ 
+
+function fight() {
+    window.alert("The fight has begun!");
+  }
+
+ // fight();
