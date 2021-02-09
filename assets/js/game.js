c770bfc652024237cca0c3c45bc05a0dfacde317
dakota <kotalilyy@gmail.com>
2021-02-09 06:26:42 -0600

    change to getPlayerName(),

--- assets/js/game.js
+++ assets/js/game.js
@@ -190,7 +190,7 @@ var randomNumber = function (min, max) {
 
 // player information
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName (),
   health: 100,
   attack: 10,
   money: 10,
