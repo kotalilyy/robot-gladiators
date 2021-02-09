1c82d62293839893859f0ca04eae3009b4fced73
dakota <kotalilyy@gmail.com>
2021-02-09 10:33:51 -0600

    add . at end of " health left "

--- assets/js/game.js
+++ assets/js/game.js
@@ -142,7 +142,7 @@ var fight = function(enemy) {
         // leave while() loop if player is dead
         break;
       } else {
-        window.alert(playerInfo.name + " still has " + playerInfo.health + " health left ");
+        window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
       }
     }
     // switch turn order for next round
