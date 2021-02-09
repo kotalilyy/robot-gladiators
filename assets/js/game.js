e5f6a269921d0e8001579a2f6cb077aeb1728f79
dakota <kotalilyy@gmail.com>
2021-02-09 09:59:04 -0600

    fix spacing

--- assets/js/game.js
+++ assets/js/game.js
@@ -182,7 +182,7 @@ var getPlayerName = function () {
 
 
 //function to generate a random numeric value 
-var randomNumber = function (min, max) {
+var randomNumber = function(min, max) {
   var value = Math.floor(Math.random() * (max - min) + min);
 
   return value;
