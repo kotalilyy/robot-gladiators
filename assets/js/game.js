1b680648752868bd21949f04c0c9a0491b94a254
dakota <kotalilyy@gmail.com>
2021-02-09 10:48:48 -0600

    fix shopOptionPrompt quote

--- assets/js/game.js
+++ assets/js/game.js
@@ -154,7 +154,7 @@ var fight = function(enemy) {
 var shop = function() {
   //ask player what they'd like to do
   var shopOptionPrompt = window.prompt(
-    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one "REFILL", "UPGRADE", or "LEAVE" to make a choice."
+    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
   );
 
   // check if prompt answer was left blank, player hit "cancel", or provided a number instead
