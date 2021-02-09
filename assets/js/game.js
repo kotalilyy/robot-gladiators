f0a4f9758e3748e003e25cca1d37638b283926ca
dakota <kotalilyy@gmail.com>
2021-02-09 10:12:01 -0600

    change '' to "" in window.alert

--- assets/js/game.js
+++ assets/js/game.js
@@ -13,10 +13,13 @@ var startGame = function () {
 
   //fight each enemy robot by looping over them and fighting them one at a time
   for (var i = 0; i < enemyInfo.length; i++) {
+    //check player stats
+    console.log(playerInfo);
+
     // if player is still alive, keep fighting the next enemy
     if (playerInfo.health > 0) {
       //let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
-      window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
+      window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
 
       // pick new enemy to fight based on the index of the enemyInfo array
       var pickedEnemyObj = enemyInfo[i];
